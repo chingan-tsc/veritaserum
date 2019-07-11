894a78839a7cbef5ad6d64219b0df7df4374a126
Lee Chi Ngan <chingan.lee90@gmail.com>
2019-07-11 10:49:57 +0800

    chore: Minor documentation change and code rearranging

--- lib/veritaserum.ex
+++ lib/veritaserum.ex
@@ -1,6 +1,6 @@
 defmodule Veritaserum do
   @moduledoc """
-  Sentiment analisis based on AFINN-165, emojis and some enhancements.
+  Sentiment analysis based on AFINN-165, emojis and some enhancements.
 
   Also supports:
   - emojis (❤️, 😱...)
@@ -10,13 +10,6 @@ defmodule Veritaserum do
 
   alias Veritaserum.Evaluator
 
-  @spec analyze(list(String.t()) | String.t()) :: list(integer) | integer
-  def analyze(input) when is_list(input) do
-    input
-    |> Stream.map(&analyze/1)
-    |> Enum.to_list()
-  end
-
   @doc """
   Returns a sentiment value for the given text
 
@@ -25,6 +18,26 @@ defmodule Veritaserum do
 
       iex> Veritaserum.analyze("I love Veritaserum")
       3
+  """
+  @spec analyze(list(String.t()) | String.t()) :: list(integer) | integer
+  def analyze(input) when is_list(input) do
+    input
+    |> Stream.map(&analyze/1)
+    |> Enum.to_list()
+  end
+
+  def analyze(input) do
+    {score, _} = analyze(input, return: :score_and_marks)
+
+    score
+  end
+
+  @doc """
+  Returns a tuple of the sentiment value and the metadata for the given text
+
+      iex> Veritaserum.analyze("I love Veritaserum", return: :score_and_marks)
+      {3, [{:neutral, 0, "i"}, {:word, 3, "love"}, {:neutral, 0, "veritaserum"}]}
+      
   """
   @spec analyze(String.t(), return: :score_and_marks) :: {number(), [{atom, number, String.t()}]}
   def analyze(input, return: :score_and_marks) do
@@ -39,12 +52,16 @@ defmodule Veritaserum do
     {score, list_with_marks}
   end
 
-  def analyze(input) do
-    {score, _} = analyze(input, return: :score_and_marks)
-
-    score
+  # Mark every word in the input with type and score
+  defp mark_word(word) do
+    with {_, nil, _} <- {:negator, Evaluator.evaluate_negator(word), word},
+         {_, nil, _} <- {:booster, Evaluator.evaluate_booster(word), word},
+         {_, nil, _} <- {:emoticon, Evaluator.evaluate_emoticon(word), word},
+         {_, nil, _} <- {:word, Evaluator.evaluate_word(word), word},
+         do: {:neutral, 0, word}
   end
 
+  # Compute the score from a list of marked words
   defp get_score(words) do
     words
     |> Stream.chunk_every(2, 1)
@@ -64,15 +81,6 @@ defmodule Veritaserum do
       end
     end)
     |> Enum.sum()
-  end
-
-  # Mark every word in the input with type and score
-  defp mark_word(word) do
-    with {_, nil, _} <- {:negator, Evaluator.evaluate_negator(word), word},
-         {_, nil, _} <- {:booster, Evaluator.evaluate_booster(word), word},
-         {_, nil, _} <- {:emoticon, Evaluator.evaluate_emoticon(word), word},
-         {_, nil, _} <- {:word, Evaluator.evaluate_word(word), word},
-         do: {:neutral, 0, word}
   end
 
   # Clean and sanitize the input text
